5d1afd487d1728ef1c8e266f4ba2ebff448f772b
Arturo Guerra <ar2roguerra@gmail.com>
2022-07-17 22:27:48 -0500

    Removed test bin

--- test.py
+++
@@ -1,7 +0,0 @@
-#a = bytearray([0xEA] * 128 * 255)
-#a = bytearray([0xEA] * 128 * 256)
-a = bytearray([0xEA] * 0x8000)
-#a = bytearray([0xEA] * 0x10000)
-
-with open("test.bin", "wb") as f:
-    f.write(a)
